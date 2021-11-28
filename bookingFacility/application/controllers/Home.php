c26bb99529caf61799313a1b242b98cce87764d9
Vallencius <vallencius1901@gmail.com>
2021-11-28 13:23:58 +0700

    Register + Logic

--- bookingFacility/application/controllers/Home.php
+++ bookingFacility/application/controllers/Home.php
@@ -2,8 +2,47 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
+    function __construct()
+	{
+		parent::__construct();
+		$this->load->model('auth');
+	}
+
 	public function index()
 	{
-        
+        $this->load->view('landing'); //Buat landing page ntar
 	}
+
+    //Buka view register
+    public function register(){ 
+        $data['style'] = $this->load->view('include/style',NULL,TRUE);
+        $this->load->view('pages/register', $data);
+    }
+
+    //Cek Rules Register
+    public function registCheck()
+	{
+		$this->form_validation->set_rules('username', 'username','trim|required|min_length[1]|max_length[255]|is_unique[user.username]');
+		$this->form_validation->set_rules('email', 'email','trim|required|min_length[1]|max_length[255]|valid_email');
+		$this->form_validation->set_rules('password', 'password','trim|required|min_length[1]|max_length[255]');
+		if ($this->form_validation->run()==true) //Kalau sesuai rules insert ke DB
+	   	{
+			$username = $this->input->post('username');
+			$email = $this->input->post('email');
+			$password = $this->input->post('password');
+			$this->auth->register($username,$email,$password);
+			$this->session->set_flashdata('success_register','Proses Pendaftaran User Berhasil');
+			redirect('home/login'); //Terus masuk ke login
+		}
+		else //Kalau ga sesuai balik ke register + bawa validation errornya
+		{
+			$this->session->set_flashdata('error', validation_errors());
+			redirect('home/register');
+		}
+	}
+
+    //Menampilkan view login
+    public function login(){
+        $this->load->view('pages/login');
+    }
 }
---
+++ bookingFacility/application/views/pages/register.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Register</title>
+		<?php echo $style; ?>
+	</head>
+	<body>
+	<div class="container">
+		<div class="card">
+			<div class="card-header">
+				<h1 style="">Register Now!</h1>
+			</div>
+			<div class="card-body">
+				<?php 
+				if($this->session->flashdata('error') !='')
+				{
+					echo '<div class="alert alert-danger" role="alert">';
+					echo $this->session->flashdata('error');
+					echo '</div>';
+				}
+				?>
+				<form method="post" action="<?php echo base_url(); ?>index.php/home/registCheck">
+					<div class="form-group">
+						<label for="username">Username</label>
+						<input type="text" class="form-control" name="username" id="username" placeholder="Enter Username">
+					</div>
+                    <div class="form-group">
+						<label for="email">Email</label>
+						<input type="text" class="form-control" name="email" id="email" placeholder="Email">
+					</div>
+					<div class="form-group">
+						<label for="password">Password</label>
+						<input type="password" class="form-control" name="password" id="password" placeholder="Password">
+					</div>
+			
+					<button type="submit" class="btn btn-primary">Register</button>
+				</form>
+			</div>
+		</div>
+	</div>		
+	</body>
+</html>
