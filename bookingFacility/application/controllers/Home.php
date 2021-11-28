f5a382de02ca16c0e3ce9157e68e0c598cadbfc7
atras30 <atrasshalhan@gmail.com>
2021-11-28 14:24:43 +0700

    tambah fungsi home/login, footer & header view

--- bookingFacility/application/controllers/Home.php
+++ bookingFacility/application/controllers/Home.php
@@ -1,48 +1,58 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Home extends CI_Controller {
-    function __construct()
-	{
-		parent::__construct();
-		$this->load->model('auth');
-	}
+  function __construct() {
+    parent::__construct();
+    $this->load->model('auth');
+    $this->load->library('form_validation');
+    $this->load->library('session');
+  }
 
-	public function index()
-	{
-        $this->load->view('home'); //Buat landing page ntar
-	}
+  public function index() {
+    $this->load->view('home'); //Buat landing page ntar
+  }
 
-    //Buka view register
-    public function register(){ 
-        $data['style'] = $this->load->view('include/style',NULL,TRUE);
-        $this->load->view('pages/register', $data);
+  //Buka view register
+  public function register() {
+    $data['style'] = $this->load->view('include/style', NULL, TRUE);
+    $this->load->view('pages/register', $data);
+  }
+
+  //Cek Rules Register
+  public function registCheck() {
+    $this->form_validation->set_rules('username', 'username', 'trim|required|min_length[1]|max_length[255]|is_unique[user.username]');
+    $this->form_validation->set_rules('email', 'email', 'trim|required|min_length[1]|max_length[255]|valid_email');
+    $this->form_validation->set_rules('password', 'password', 'trim|required|min_length[1]|max_length[255]');
+    if ($this->form_validation->run() == true) //Kalau sesuai rules insert ke DB
+    {
+      $username = $this->input->post('username');
+      $email = $this->input->post('email');
+      $password = $this->input->post('password');
+      $this->auth->register($username, $email, $password);
+      $this->session->set_flashdata('success_register', 'Proses Pendaftaran User Berhasil');
+      redirect('home/login'); //Terus masuk ke login
+    } else //Kalau ga sesuai balik ke register + bawa validation errornya
+    {
+      $this->session->set_flashdata('error', validation_errors());
+      redirect('home/register');
+    }
+  }
+
+  //Menampilkan view login
+  public function login() {
+    $loginStatus = 0;
+    $this->form_validation->set_rules('email', 'Email', "valid_email");
+
+    if ($this->input->post('email') != null && $this->input->post('password') != NULL) {
+      $loginStatus = $this->auth->login($this->input->post('email'), $this->input->post('password'));
     }
 
-    //Cek Rules Register
-    public function registCheck()
-	{
-		$this->form_validation->set_rules('username', 'username','trim|required|min_length[1]|max_length[255]|is_unique[user.username]');
-		$this->form_validation->set_rules('email', 'email','trim|required|min_length[1]|max_length[255]|valid_email');
-		$this->form_validation->set_rules('password', 'password','trim|required|min_length[1]|max_length[255]');
-		if ($this->form_validation->run()==true) //Kalau sesuai rules insert ke DB
-	   	{
-			$username = $this->input->post('username');
-			$email = $this->input->post('email');
-			$password = $this->input->post('password');
-			$this->auth->register($username,$email,$password);
-			$this->session->set_flashdata('success_register','Proses Pendaftaran User Berhasil');
-			redirect('home/login'); //Terus masuk ke login
-		}
-		else //Kalau ga sesuai balik ke register + bawa validation errornya
-		{
-			$this->session->set_flashdata('error', validation_errors());
-			redirect('home/register');
-		}
-	}
 
-    //Menampilkan view login
-    public function login(){
-        $this->load->view('pages/login');
+    if (!$this->form_validation->run() || !$loginStatus) {
+      $this->load->view('pages/login'); //login gagal
+    } else {
+      redirect("pages/landing_page");
     }
+  }
 }
