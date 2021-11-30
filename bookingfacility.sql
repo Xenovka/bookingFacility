44b541fed4bc31e607cccb20e2a621dc6234ba3c
Vallencius <vallencius1901@gmail.com>
2021-11-30 12:43:17 +0700

    Update DB tambah Facility Table

--- bookingfacility.sql
+++ bookingfacility.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2021 at 07:23 AM
+-- Generation Time: Nov 30, 2021 at 06:42 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -24,6 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `facility`
+--
+
+CREATE TABLE `facility` (
+  `FacilityID` int(11) NOT NULL,
+  `FacilityName` varchar(255) NOT NULL,
+  `Image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `facility`
+--
+
+INSERT INTO `facility` (`FacilityID`, `FacilityName`, `Image`) VALUES
+(1, 'Colaborative Lab', '/assets/images/colaborative_lab.jpg'),
+(2, 'Lab Computer', '/assets/images/lab_computer.jpg'),
+(3, 'Lecture Theater', '/assets/images/lecture_theater.jpg'),
+(4, 'Meeting Room', '/assets/images/meeting_room.jpg'),
+(5, 'Studio Greenscreen', '/assets/images/studio_greenscreen.jpg'),
+(6, 'Studio Photo', '/assets/images/studio_photo.jpg');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -31,19 +55,26 @@ CREATE TABLE `user` (
   `UserID` int(11) NOT NULL,
   `Username` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL
+  `Password` varchar(255) NOT NULL,
+  `Role` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`UserID`, `Username`, `Email`, `Password`) VALUES
-(1, 'agung123', 'agung@umn.ac.id', '$2y$10$2UCelXL./iuwRY0Nv6NuPuJcnBy/nfsSOrnsGJaI1./baKzz2OPNe');
+INSERT INTO `user` (`UserID`, `Username`, `Email`, `Password`, `Role`) VALUES
+(1, 'agung123', 'agung@umn.ac.id', '$2y$10$2UCelXL./iuwRY0Nv6NuPuJcnBy/nfsSOrnsGJaI1./baKzz2OPNe', 'Admin');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `facility`
+--
+ALTER TABLE `facility`
+  ADD PRIMARY KEY (`FacilityID`);
 
 --
 -- Indexes for table `user`
@@ -56,10 +87,16 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `facility`
+--
+ALTER TABLE `facility`
+  MODIFY `FacilityID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
