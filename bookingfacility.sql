080c1c033f3017d98327d20c825836d864e9b3c9
Vallencius <vallencius1901@gmail.com>
2021-11-30 20:25:40 +0700

    Update bookingfacility.sql

--- bookingfacility.sql
+++ bookingfacility.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 30, 2021 at 06:42 AM
+-- Generation Time: Nov 30, 2021 at 02:25 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -38,12 +38,55 @@ CREATE TABLE `facility` (
 --
 
 INSERT INTO `facility` (`FacilityID`, `FacilityName`, `Image`) VALUES
-(1, 'Colaborative Lab', '/assets/images/colaborative_lab.jpg'),
-(2, 'Lab Computer', '/assets/images/lab_computer.jpg'),
-(3, 'Lecture Theater', '/assets/images/lecture_theater.jpg'),
-(4, 'Meeting Room', '/assets/images/meeting_room.jpg'),
-(5, 'Studio Greenscreen', '/assets/images/studio_greenscreen.jpg'),
-(6, 'Studio Photo', '/assets/images/studio_photo.jpg');
+(1, 'Colaborative Lab', 'colaborative_lab.jpg'),
+(2, 'Lab Computer', 'lab_computer.jpg'),
+(3, 'Lecture Theater', 'lecture_theater.jpg'),
+(4, 'Meeting Room', 'meeting_room.jpg'),
+(5, 'Studio Greenscreen', 'studio_greenscreen.jpg'),
+(6, 'Studio Photo', 'studio_photo.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests`
+--
+
+CREATE TABLE `requests` (
+  `RequestID` int(11) NOT NULL,
+  `RequesterID` int(11) NOT NULL,
+  `ReqFacilityID` int(11) NOT NULL,
+  `Date` date NOT NULL,
+  `StartTime` time NOT NULL,
+  `EndTime` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `requests`
+--
+
+INSERT INTO `requests` (`RequestID`, `RequesterID`, `ReqFacilityID`, `Date`, `StartTime`, `EndTime`) VALUES
+(1, 3, 4, '2021-12-03', '13:29:00', '16:29:00'),
+(2, 1, 3, '2021-12-03', '19:00:00', '21:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `RoleID` int(11) NOT NULL,
+  `RoleName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`RoleID`, `RoleName`) VALUES
+(1, 'Admin'),
+(2, 'Management'),
+(3, 'User');
 
 -- --------------------------------------------------------
 
@@ -56,7 +99,7 @@ CREATE TABLE `user` (
   `Username` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL,
   `Password` varchar(255) NOT NULL,
-  `Role` varchar(255) NOT NULL
+  `Role` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -64,7 +107,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`UserID`, `Username`, `Email`, `Password`, `Role`) VALUES
-(1, 'agung123', 'agung@umn.ac.id', '$2y$10$2UCelXL./iuwRY0Nv6NuPuJcnBy/nfsSOrnsGJaI1./baKzz2OPNe', 'Admin');
+(1, 'agung123', 'agung@umn.ac.id', '$2y$10$2UCelXL./iuwRY0Nv6NuPuJcnBy/nfsSOrnsGJaI1./baKzz2OPNe', 1),
+(3, 'Vallencius', 'vallencius.siswanto@student.umn.ac.id', '$2y$10$CRUpdchAR9sLJa8AUczWEu2TjnV4SUtxcjMIoqVPaIdfc09R5gFaa', 1),
+(4, 'Vallencius111', 'admin@umn.ac.id', '$2y$10$TFqJB.YMtoZr.TwIovywduvkaeHHe5nc1nn5coypaJub6ljfoEjZq', 2),
+(6, 'CobaCoba', 'vallencius1901@gmail.com', '$2y$10$Ca847wTzkmk20pItyT0mUOwrdxYEsXwtn74A.v6CzF5PmxNX//qki', 3);
 
 --
 -- Indexes for dumped tables
@@ -77,10 +123,25 @@ ALTER TABLE `facility`
   ADD PRIMARY KEY (`FacilityID`);
 
 --
+-- Indexes for table `requests`
+--
+ALTER TABLE `requests`
+  ADD PRIMARY KEY (`RequestID`),
+  ADD KEY `ReqFacilityID` (`ReqFacilityID`),
+  ADD KEY `RequesterID` (`RequesterID`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`RoleID`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`UserID`);
+  ADD PRIMARY KEY (`UserID`),
+  ADD KEY `Role` (`Role`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -90,13 +151,36 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `facility`
 --
 ALTER TABLE `facility`
-  MODIFY `FacilityID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `FacilityID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `requests`
+--
+ALTER TABLE `requests`
+  MODIFY `RequestID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `requests`
+--
+ALTER TABLE `requests`
+  ADD CONSTRAINT `requests_ibfk_1` FOREIGN KEY (`ReqFacilityID`) REFERENCES `facility` (`FacilityID`),
+  ADD CONSTRAINT `requests_ibfk_2` FOREIGN KEY (`RequesterID`) REFERENCES `user` (`UserID`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`Role`) REFERENCES `role` (`RoleID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
