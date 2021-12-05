c3fece987f215e5ceec5003d16f0c57c2910b18a
unknown <antoniuskevin15@gmail.com>
2021-12-05 22:26:54 +0700

    rapi rapi aja ges

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -102,7 +102,8 @@ class Home extends CI_Controller {
           if($captcha_response != '')
           {
             $keySecret = '6LdnJXwdAAAAADJSD9sHpyWzJzY_8Bnf1HITyfal';
-            // $keySecret = '6Lf6Cm8dAAAAAM3xM1v2kY9ichIo9tsyTMW9tsuw'; --> untuk Localhost
+            // key untuk localhost
+            // $keySecret = '6Lf6Cm8dAAAAAM3xM1v2kY9ichIo9tsyTMW9tsuw'; 
 
             $check = array(
               'secret'		=>	$keySecret,
